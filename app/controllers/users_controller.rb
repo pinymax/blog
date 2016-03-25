c83c01b3ed372e52918761f78270dfeaa06d5557
Max Obushenko <max.obushenko@gmail.com>
2016-03-25 14:28:05 +0200

    Added avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confitmaion)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 
 
