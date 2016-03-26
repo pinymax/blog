b6bd8046921e8c9dc4312f92aa1ce3b30bbd267e
Max Obushenko <max.obushenko@gmail.com>
2016-03-26 17:35:48 +0200

    improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, :email, :password_digest , presence: true
   validates :password, confirmation: true
+  
   validates :email, :uniqueness => { :case_sensitive => false }
   has_secure_password
   has_many :posts
