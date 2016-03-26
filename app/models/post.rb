580986c661842a1b952ae7718bec2cb1896fb51a
Max Obushenko <max.obushenko@gmail.com>
2016-03-26 16:19:07 +0200

    Fixed deploy problems

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
-  validates :name, :email, :password_digest , presence: true
   has_many :comments, dependent: :destroy
   belongs_to :user
   validates :title,  presence: true
   validates_length_of :title, :minimum => 1, :maximum => 20
-  validates :body,  presence: true 
+  validates :body,  presence: true
 end
