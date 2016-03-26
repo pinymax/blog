af66d80be51f0a63a6c564973d3eb63a159fa631
Max Obushenko <max.obushenko@gmail.com>
2016-03-26 12:50:20 +0200

    fixed comment styles

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,7 +2,7 @@ module CommentsHelper
 
 def nested_comments(comments)
   comments.map do |comment, sub_comments|
-    render(comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_comments")
+    content_tag(:div, render(comment), :class => "media")
   end.join.html_safe
 end
 
