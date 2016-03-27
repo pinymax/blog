b078b44d60bebd59f8a24ad146c386c12b00956f
Max Obushenko <max.obushenko@gmail.com>
2016-03-27 15:28:29 +0300

    added nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,8 @@
 class CommentsController < ApplicationController
 
   def reply
-    @par = Comment.find(params[:comment_id])
     @post = Post.find(params[:post_id])
-    comment = Comment.new(:parent_id => params[:comment_id])
+    @comment = @post.comments.find(params[:comment_id]).children.new
   end
 
   def create
@@ -31,12 +30,12 @@ class CommentsController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-      redirect_to post_path(@post) 
+      redirect_to post_path(@post)
   end
 
 private
 
   def comment_params
-    params.require(:comment).permit(:name, :body, :parent_id, :comment_id)
+    params.require(:comment).permit(:name, :body, :ancestry)
   end
 end
